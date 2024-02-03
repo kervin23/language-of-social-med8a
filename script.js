e972d1dfc03eb12e3b06d4ea9edd71ad62dccb63
kervin23 <143016686+kervin23@users.noreply.github.com>
2024-02-03 13:33:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+function showcategory1(){
+    document.getElementById("category1").classList.add("show")
+    document.getElementById("category2").classList.remove("show")
+    document.getElementById("category3").classList.remove("show")
+    document.getElementById("category4").classList.remove("show")
+    document.getElementById("category5").classList.remove("show")
+
+    var myElement = document.getElementById('category1Button');
+    var otherElement = document.getElementById('category2Button');
+    var otherElement2 = document.getElementById('category3Button');
+    var otherElement3 = document.getElementById('category4Button');
+    var otherElement4 = document.getElementById('category5Button')
+
+
+    otherElement.style.backgroundColor  = 'rgb(255,255,255)'
+    otherElement2.style.backgroundColor = 'rgb(255,255,255)'
+    otherElement3.style.backgroundColor = 'rgb(255,255,255)'
+    otherElement4.style.backgroundColor = 'rgb(255,255,255)'
+
+    otherElement.style.fontSize  = '12px';
+    otherElement2.style.fontSize = '12px';
+    otherElement3.style.fontSize = '12px';
+    otherElement4.style.fontSize = '12px';
+    
+
+
+    myElement.style.backgroundColor = 'rgb(246, 132, 158)';
+    myElement.style.fontSize = '24px';
+
+    
+
+    }
+function showcategory2(){
+    document.getElementById("category1").classList.remove("show")
+    document.getElementById("category2").classList.add("show")
+    document.getElementById("category3").classList.remove("show")
+    document.getElementById("category4").classList.remove("show")
+    document.getElementById("category5").classList.remove("show")
+
+    var myElement = document.getElementById('category2Button');
+    var otherElement = document.getElementById('category1Button');
+    var otherElement2 = document.getElementById('category3Button');
+    var otherElement3 = document.getElementById('category4Button');
+    var otherElement4 = document.getElementById('category5Button')
+
+
+    otherElement.style.backgroundColor  = 'rgb(255,255,255)'
+    otherElement2.style.backgroundColor = 'rgb(255,255,255)'
+    otherElement3.style.backgroundColor = 'rgb(255,255,255)'
+    otherElement4.style.backgroundColor = 'rgb(255,255,255)'
+
+    otherElement.style.fontSize  = '12px';
+    otherElement2.style.fontSize = '12px';
+    otherElement3.style.fontSize = '12px';
+    otherElement4.style.fontSize = '12px';
+    
+    
+
+    myElement.style.backgroundColor = 'rgb(246, 132, 158)';
+    myElement.style.fontSize = '24px';
+
+    
+
+    }
+function showcategory3(){
+    document.getElementById("category1").classList.remove("show")
+    document.getElementById("category2").classList.remove("show")
+    document.getElementById("category3").classList.add("show")
+    document.getElementById("category4").classList.remove("show")
+    document.getElementById("category5").classList.remove("show")
+
+
+    var myElement = document.getElementById('category3Button');
+    var otherElement = document.getElementById('category2Button');
+    var otherElement2 = document.getElementById('category1Button');
+    var otherElement3 = document.getElementById('category4Button');
+    var otherElement4 = document.getElementById('category5Button')
+
+    otherElement.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement2.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement3.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement4.style.backgroundColor= 'rgb(255,255,255)'
+
+    otherElement.style.fontSize = '12px';
+    otherElement2.style.fontSize = '12px';
+    otherElement3.style.fontSize = '12px';
+    otherElement4.style.fontSize = '12px';
+    
+
+
+
+    myElement.style.backgroundColor = 'rgb(246, 132, 158)';
+    myElement.style.fontSize = '24px';
+
+    }
+
+
+    function showcategory4(){
+    document.getElementById("category1").classList.remove("show")
+    document.getElementById("category2").classList.remove("show")
+    document.getElementById("category3").classList.remove("show")
+    document.getElementById("category4").classList.add("show")
+    document.getElementById("category5").classList.remove("show")
+
+    var myElement = document.getElementById('category4Button');
+    var otherElement = document.getElementById('category3Button');
+    var otherElement2 = document.getElementById('category2Button');
+    var otherElement3 = document.getElementById('category1Button');
+    var otherElement4 = document.getElementById('category5Button')
+
+
+    otherElement.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement2.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement3.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement4.style.backgroundColor= 'rgb(255,255,255)'
+
+    otherElement.style.fontSize = '12px';
+    otherElement2.style.fontSize = '12px';
+    otherElement3.style.fontSize = '12px';
+    otherElement4.style.fontSize = '12px';
+    
+
+
+
+    myElement.style.backgroundColor = 'rgb(246, 132, 158)';
+    myElement.style.fontSize = '24px';
+
+    }
+
+    function showcategory5(){
+    document.getElementById("category1").classList.remove("show")
+    document.getElementById("category2").classList.remove("show")
+    document.getElementById("category3").classList.remove("show")
+    document.getElementById("category4").classList.remove("show")
+    document.getElementById("category5").classList.add("show")
+    
+
+    var myElement = document.getElementById('category5Button');
+    var otherElement = document.getElementById('category4Button');
+    var otherElement2 = document.getElementById('category3Button');
+    var otherElement3 = document.getElementById('category2Button');
+    var otherElement4 = document.getElementById('category1Button')
+
+
+    otherElement.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement2.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement3.style.backgroundColor= 'rgb(255,255,255)'
+    otherElement4.style.backgroundColor= 'rgb(255,255,255)'
+
+    otherElement.style.fontSize = '12px';
+    otherElement2.style.fontSize = '12px';
+    otherElement3.style.fontSize = '12px';
+    otherElement4.style.fontSize = '12px';
+    
+
+
+    myElement.style.backgroundColor = 'rgb(246, 132, 158)';
+    myElement.style.fontSize = '24px';
+
+    }
+
